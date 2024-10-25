2e01e4eebfee598b61eb1a460d3f53b9ed0d9da7
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-10-25 10:27:19 +0300

    Update 1.c

--- 2nd Semester/Object Oriented Programming/labs/1st lab/app/1.c
+++ 2nd Semester/Object Oriented Programming/labs/1st lab/app/1.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+//& address
+//in functie cu * pt adresa = adresa spre care arata valoarea
+//* pointer spre
+//** dublu pointer =
+//typedef struct student Stud
+//sizeof
+int main() {
+    printf("Hello World!\n");
+    int n, sum=0, m;
+    printf("Introduceti un numar: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        scanf("%d", &m);
+        sum = sum + m;
+    }
+    printf("%d", sum);
+    return 0;
+}
