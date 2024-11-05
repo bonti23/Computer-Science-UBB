934a899146e6400c172f7edb702f265598a7396a
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-05 07:19:30 +0200

    Update client.c

--- 3rd Semester/Computer Networks/labs/1st homework/5th exercise/client.c
+++ 3rd Semester/Computer Networks/labs/1st homework/5th exercise/client.c
@@ -1,1 +1,41 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+ 
+int main() {
+  int c;
+  struct sockaddr_in server;
+  uint16_t n;
+  char divizori[100];
+  
+  c = socket(AF_INET, SOCK_STREAM, 0);
+  if (c < 0) {
+    printf("Eroare la crearea socketului client\n");
+    return 1;
+  }
+  
+  memset(&server, 0, sizeof(server));
+  server.sin_port = htons(1234);
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  
+  if (connect(c, (struct sockaddr *) &server, sizeof(server)) < 0) {
+    printf("Eroare la conectarea la server\n");
+    return 1;
+  }
+  
+  printf("numarul e: ");
+  scanf("%hu", &n);
+  n=htons(n);
+  send(c, &n, sizeof(n), 0);
+  recv(c, divizori, sizeof(divizori), 0);
+  printf("divizorii sunt: %s\n", divizori);
+  close(c);
+  return 0;
+}
 
