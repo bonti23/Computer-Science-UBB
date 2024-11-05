53fe83d43baeee4b7aea45a2640a9a068b1bff63
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-05 06:39:32 +0200

    Create server.c

---
+++ 3rd Semester/Computer Networks/labs/1st homework/3rd exercise/server.c
@@ -0,0 +1,55 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <unistd.h>
+ 
+int main() {
+  int s;
+  struct sockaddr_in server, client;
+  int c, l;
+  
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    printf("Eroare la crearea socketului server\n");
+    return 1;
+  }
+  
+  memset(&server, 0, sizeof(server));
+  server.sin_port = htons(1234);
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  
+  if (bind(s, (struct sockaddr *) &server, sizeof(server)) < 0) {
+    printf("Eroare la bind\n");
+    return 1;
+  }
+ 
+  listen(s, 5);
+  
+  l = sizeof(client);
+  memset(&client, 0, sizeof(client));
+  
+  while (1) {
+    char sir[100];
+    char oglindit[100];
+    c = accept(s, (struct sockaddr *) &client, &l);
+    printf("S-a conectat un client.\n");
+    // deservirea clientului
+    recv(c, &sir, sizeof(sir), 0);
+
+    int len=strlen(sir);
+    int j=0;
+    for(int i=len-1; i>=0; i--){
+        oglindit[j]=sir[i];
+        j++;
+    }
+    send(c, oglindit, strlen(oglindit)+1, 0);
+    close(c);
+    // sfarsitul deservirii clientului;
+  }
+  close(s);
+  return 0;
+}
