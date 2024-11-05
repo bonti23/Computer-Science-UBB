6f18be02a891904eb99183ff12682861deeaf81c
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-05 07:20:03 +0200

    Update server.c

--- 3rd Semester/Computer Networks/labs/1st homework/5th exercise/server.c
+++ 3rd Semester/Computer Networks/labs/1st homework/5th exercise/server.c
@@ -1,1 +1,57 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <unistd.h>
+ 
+int main() {
+  int s;
+  struct sockaddr_in server, client;
+  int c, l;
+  
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    printf("Eroare la crearea socketului server\n");
+    return 1;
+  }
+  
+  memset(&server, 0, sizeof(server));
+  server.sin_port = htons(1234);
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  
+  if (bind(s, (struct sockaddr *) &server, sizeof(server)) < 0) {
+    printf("Eroare la bind\n");
+    return 1;
+  }
+ 
+  listen(s, 5);
+  
+  l = sizeof(client);
+  memset(&client, 0, sizeof(client));
+  
+  while (1) {
+    uint16_t n;
+    char divizori[256];
+    char buffer[10];
+    c = accept(s, (struct sockaddr *) &client, &l);
+    printf("S-a conectat un client.\n");
+    // deservirea clientului
+    recv(c, &n, sizeof(n), MSG_WAITALL);
+    n=ntohs(n);
+    for(int i=1; i<=n; i++){
+        if(n%i==0){
+            sprintf(buffer, "%hu ", i);
+            strcat(divizori, buffer);
+        }
+    }
+    send(c, divizori, strlen(divizori)+1, 0);
+    close(c);
+    // sfarsitul deservirii clientului;
+  }
+  close(s);
+  return 0;
+}
 
