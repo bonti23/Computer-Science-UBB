356b301ddcebda17796ab4264d46fa928408b149
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-22 15:36:19 +0200

    Update service.h

--- 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/service.h
+++ 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/service.h
@@ -1,17 +1,11 @@
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wvisibility"
-#ifndef LAB2_4_SERVICE_H
-#define LAB2_4_SERVICE_H
-
-struct ListaMea creeazavid();
-struct oferta creare(char tip[25], int suprafata, char adresa[25], int pret);
-void adaugare(struct oferta oferta, struct ListaMea *lista);
-void validare(char erori[100], int *ok, char tip[25], int suprafata, char adresa[25], int pret);
-void validare_index(struct ListaMea *lista, int index, char erori[100], int* ok);
-void modificare(struct ListaMea *lista, int index, char tip[25], int suprafata, char adresa[25], int pret);
-void stergere(struct ListaMea *lista, int index);
-void ordonare(struct ListaMea *lista, int ordonare);
-struct ListaMea *filtrare(struct ListaMea *lista, char criteriu[20], char ce_fel[20]);
-
-#endif //LAB2_4_SERVICE_H
-#pragma clang diagnostic pop
+#ifndef INC_2ND_4TH_LAB_1ST_PART_SERVICE_H
+#define INC_2ND_4TH_LAB_1ST_PART_SERVICE_H
+struct ListaMea vid();
+struct oferta create_service(char tip[25], int suprafata, char adresa[25], int pret);
+void add_service(struct oferta oferta, struct ListaMea *lista);
+void modify_service(struct ListaMea *lista, int index, char tip[25], int suprafata, char adresa[25], int pret);
+void delete_service(struct ListaMea *lista, int index);
+struct ListaMea *filtrare_service(struct ListaMea* lista, char criteriu[25], char tipul[25]);
+void ordonare_service(struct ListaMea* lista, char ordonare[20]);
+void validate_service(char erori[100], char tip[25], int suprafata, char adresa[25], int pret);
+#endif //INC_2ND_4TH_LAB_1ST_PART_SERVICE_H
