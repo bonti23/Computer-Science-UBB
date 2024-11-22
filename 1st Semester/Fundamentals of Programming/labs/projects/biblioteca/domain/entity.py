7b8db1eface58d7d8036d835b1e0ef8babd3c0b2
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-22 22:12:17 +0200

    Create entity.py

---
+++ 1st Semester/Fundamentals of Programming/labs/projects/biblioteca/domain/entity.py
@@ -0,0 +1,30 @@
+class Carte:
+    def __init__(self, id, titlu, autor, anAparitie):
+        self.__id = id
+        self.__titlu = titlu
+        self.__autor = autor
+        self.__anAparitie = anAparitie
+
+    def get_id(self):
+        return self.__id
+
+    def get_titlu(self):
+        return self.__titlu
+
+    def get_autor(self):
+        return self.__autor
+
+    def get_anAparitie(self):
+        return self.__anAparitie
+
+    def set_id(self, value):
+        self.__id = value
+
+    def set_titlu(self, value):
+        self.__titlu = value
+
+    def set_autor(self, value):
+        self.__autor = value
+
+    def set_anAparitie(self, value):
+        self.__anAparitie = value
