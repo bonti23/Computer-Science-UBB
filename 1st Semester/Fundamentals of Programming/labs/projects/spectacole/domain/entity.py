be6d2c6aead075b1ae219d104ad6b9ad0c1bf804
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-22 23:02:25 +0200

    Create entity.py

---
+++ 1st Semester/Fundamentals of Programming/labs/projects/spectacole/domain/entity.py
@@ -0,0 +1,30 @@
+class Spectacol:
+    def __init__(self, titlu, artist, gen, durata):
+        self.__titlu = titlu
+        self.__artist = artist
+        self.__gen = gen
+        self.__durata = durata
+
+    def get_titlu(self):
+        return self.__titlu
+
+    def get_artist(self):
+        return self.__artist
+
+    def get_gen(self):
+        return self.__gen
+
+    def get_durata(self):
+        return self.__durata
+
+    def set_titlu(self, value):
+        self.__titlu = value
+
+    def set_artist(self, value):
+        self.__artist = value
+
+    def set_gen(self, value):
+        self.__gen = value
+
+    def set_durata(self, value):
+        self.__durata = value
