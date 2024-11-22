c24972e71cb111845f7167c7b42c1b5b12e9c736
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-22 15:35:34 +0200

    Update domain.h

--- 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/domain.h
+++ 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/domain.h
@@ -1,5 +1,6 @@
-#ifndef LAB2_4_OFERTA_H
-#define LAB2_4_OFERTA_H
+#ifndef INC_2ND_4TH_LAB_1ST_PART_DOMAIN_H
+#define INC_2ND_4TH_LAB_1ST_PART_DOMAIN_H
+
 struct oferta{
     char tip[25]; //teren, casa, apartament
     int suprafata;
@@ -11,4 +12,5 @@ struct ListaMea{
     struct oferta lista_oferte[100];
     int lungime;
 };
-#endif //LAB2_4_OFERTA_H
+
+#endif //INC_2ND_4TH_LAB_1ST_PART_DOMAIN_H
