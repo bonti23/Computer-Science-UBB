c56a43f971bb9f7523ca684f94c75b85c15a3480
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-22 15:35:19 +0200

    Update domain.c

--- 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/domain.c
+++ 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/domain.c
@@ -1,14 +1,3 @@
+#include "domain.h"
 #include <string.h>
 #include <stdlib.h>
-
-struct oferta{
-    char tip[25]; //teren, casa, apartament
-    int suprafata;
-    char adresa[25];
-    int pret;
-};
-
-struct ListaMea{
-    struct oferta lista_oferte[100];
-    int lungime;
-};
