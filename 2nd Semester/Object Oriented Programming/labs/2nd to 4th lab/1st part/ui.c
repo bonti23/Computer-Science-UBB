fb16d4d6bfb1e9b0b3f1b3552cf1bfda3bbae7f9
Alexandra Bontidean <155551124+bonti23@users.noreply.github.com>
2024-11-22 15:36:48 +0200

    Update ui.c

--- 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/ui.c
+++ 2nd Semester/Object Oriented Programming/labs/2nd to 4th lab/1st part/ui.c
@@ -1,32 +1,27 @@
+#include "ui.h"
 #include "service.h"
 #include "domain.h"
 #include "stdio.h"
 #include "stdlib.h"
-#include "teste.h"
+#include "string.h"
 
-void afisare_lista_oferte(struct ListaMea *lista){
+void afiseaza_oferte(struct ListaMea* lista){
     struct oferta oferta;
-    int index = lista->lungime;
-    printf("Lista: \n");
-    for (int i=0; i<index; i++){
-        oferta = lista-> lista_oferte[i];
-        printf("tipul: %s; suprafata: %d; adresa: %s; pretul: %ld", oferta.tip, oferta.suprafata, oferta.adresa, oferta.pret);
-        printf("\n");
+    for(int i=0; i<lista->lungime; i++){
+        oferta=lista->lista_oferte[i];
+        printf("%s  %d  %s  %d\n", oferta.tip, oferta.suprafata, oferta.adresa, oferta.pret);
     }
 }
-
-void menu(){
-    printf("Agentie de imobiliare! \n");
+void meniu(){
+    printf("Agentie!\n");
     printf("1. Adauga o oferta noua.\n");
     printf("2. Modifica oferta.\n");
     printf("3. Sterge oferta.\n");
     printf("4. Filtrare oferte.\n");
     printf("5. Oferte ordonate.\n");
     printf("6. Inchide aplicatia.\n");
-    printf("Alegeti optiunea: \n");
 }
-
-void creare_oferta_ui(struct ListaMea *lista){
+void creare_ui(struct ListaMea* lista){
     char tip[25];
     int suprafata;
     char adresa[25];
@@ -39,114 +34,112 @@ void creare_oferta_ui(struct ListaMea *lista){
     scanf("%s", adresa);
     printf("Pretul: ");
     scanf("%d", &pret);
-
     char erori[100];
-    int ok=1;
-    validare(erori, &ok, tip, suprafata, adresa, pret);
-    if (ok==0)
+    validate_service(erori, tip, suprafata, adresa, pret);
+    if (strcmp(erori, "")!=0)
         printf("%s", erori);
     else{
-        struct oferta oferta = creare(tip, suprafata, adresa, pret);
-        adaugare(oferta, lista);
-        afisare_lista_oferte(lista);
+        struct oferta oferta = create_service(tip, suprafata, adresa, pret);
+        add_service(oferta, lista);
+        afiseaza_oferte(lista);
     }
 }
 
-void modificare_oferta_ui(struct ListaMea *lista){
+void modificare_ui(struct ListaMea* lista){
+    int ok=1;
     int index;
     char tip[25];
     int suprafata;
     char adresa[25];
     int pret;
-
     printf("Indexul ofertei de actualizat: ");
-    scanf("%d", &index);
+    scanf("%d",&index);
     printf("Tipul nou: ");
-    scanf("%s", tip);
+    scanf("%s",tip);
     printf("Suprafata noua: ");
-    scanf("%d", &suprafata);
+    scanf("%d",&suprafata);
     printf("Adresa noua: ");
-    scanf("%s", adresa);
+    scanf("%s",adresa);
     printf("Pretul nou: ");
-    scanf("%d", &pret);
+    scanf("%d",&pret);
 
     char erori[100];
-    int ok = 1;
+    validate_service(erori, tip, suprafata, adresa, pret);
 
-    validare_index(lista, index, erori, &ok);
-    validare(erori, &ok, tip, suprafata, adresa, pret);
-
-    if(ok == 0)
+    if(index>=lista->lungime || index<0){
+        printf("Index invalid!");
+        ok=0;
+    }
+    if(strcmp(erori, "")!=0){
         printf("%s", erori);
-    else{
-        modificare(lista, index, tip, suprafata, adresa, pret);
-        afisare_lista_oferte(lista);
+        ok=0;
+    }
+    if(ok==1){
+        modify_service(lista, index, tip, suprafata, adresa, pret);
+        afiseaza_oferte(lista);
     }
 }
-void stergere_oferta_ui(struct ListaMea *lista){
+
+void stergere_ui(struct ListaMea *lista){
     int index;
     printf("Indexul ofertei de sters: ");
     scanf("%d", &index);
-
-    char erori[100];
-    int ok = 1;
-    validare_index(lista, index, erori, &ok);
-
+    int ok=1;
+    if(index>=lista->lungime || index<0){
+        printf("Index invalid!");
+        ok=0;
+    }
     if(ok == 1){
-        stergere(lista, index);
+        delete_service(lista, index);
         if(lista->lungime != 0)
-            afisare_lista_oferte(lista);
+            afiseaza_oferte(lista);
         else
             printf("Lista este vida!\n");
     }
-    else
-        printf("%s", erori);
 }
 
-void filtrare_ui(struct ListaMea *lista){
-    char criteriu[25], ce_fel[25];
+void filtreaza_ui(struct ListaMea *lista){
+    char criteriu[25], tipul[25];
     printf("Criteriul: ");
-    scanf("%s", criteriu);
+    scanf("%s",criteriu);
     printf("Ce tip: ");
-    scanf("%s", ce_fel);
-    struct ListaMea *filtrat = filtrare(lista, criteriu, ce_fel);
-    if (filtrat == NULL) {
+    scanf("%s",tipul);
+    struct ListaMea *filtrat=filtrare_service(lista,criteriu,tipul);
+    if(filtrat==NULL){
         printf("Nu s-au gasit oferte conform criteriului.\n");
         return;
     }
     printf("Oferte filtrate:\n");
-    afisare_lista_oferte(filtrat);
-
-    free(filtrat);
+    afiseaza_oferte(filtrat);
+    free(filtrat);//dealocam
 }
 
-void ordonare_ui(struct ListaMea *lista){
-    int cum;
-    printf("Introduceti 0 pentru ordonarea crescatoare sau 1 pentru ordonara descrescatoare.\n");
+void ordoneaza_ui(struct ListaMea *lista){
+    char cum[20];
+    printf("Introduceti crescator/descrescator in functie de ordinea dorita.\n");
     printf("Optiunea: ");
-    scanf("%d", &cum);
-    ordonare(lista, cum);
-    afisare_lista_oferte(lista);
+    scanf("%s", cum);
+    ordonare_service(lista, cum);
+    afiseaza_oferte(lista);
 }
 
 void run(){
-    test_all();
-    struct ListaMea lista = creeazavid();
+    struct ListaMea lista = vid();
     char optiune;
-    menu();
+    meniu();
     while(1){
-        printf(">>> ");
-        scanf("%s", &optiune);
+        printf("Introduceti optiunea: ");
+        scanf("%s", optiune);
         if(optiune == '1')
-            creare_oferta_ui(&lista);
+            creare_ui(&lista);
         else if(optiune == '2')
-            modificare_oferta_ui(&lista);
+            modificare_ui(&lista);
         else if(optiune == '3')
-            stergere_oferta_ui(&lista);
+            stergere_ui(&lista);
         else if(optiune == '4')
-            filtrare_ui(&lista);
+            filtreaza_ui(&lista);
         else if(optiune == '5')
-            ordonare_ui(&lista);
+            ordoneaza_ui(&lista);
         else if(optiune == '6'){
             printf("Aplicatie inchisa.");
             break;
@@ -154,5 +147,4 @@ void run(){
         else
             printf("Optiune invalida!\n");
     }
-
 }
