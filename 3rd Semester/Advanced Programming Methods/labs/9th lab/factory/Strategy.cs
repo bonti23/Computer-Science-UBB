2bba79226232c3d5e1be8504148c983dc03de54b
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2024-12-09 20:16:12 +0200

    Add files via upload

---
+++ 3rd Semester/Advanced Programming Methods/labs/9th lab/factory/Strategy.cs
@@ -0,0 +1,7 @@
+﻿namespace Seminar_10;
+
+public enum Strategy
+{
+    Lifo,
+    Fifo
+}
---
+++ 3rd Semester/Advanced Programming Methods/labs/9th lab/factory/TaskContainerFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Seminar_10;
+
+public class TaskContainerFactory:Factory
+{
+
+    public IContainer CreateContainer(Strategy strategy)
+    {
+        if (strategy == Strategy.Fifo)
+        {
+            return new QueueContainer();
+        }
+        return new StackContainer(10);
+    }
+}
