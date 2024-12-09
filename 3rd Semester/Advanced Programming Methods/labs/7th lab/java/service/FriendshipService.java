c1681545aae345a8e7bd98ca6e0e90af0f780cb7
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2024-12-09 20:10:08 +0200

    Add files via upload

---
+++ 3rd Semester/Advanced Programming Methods/labs/7th lab/java/service/FriendshipService.java
@@ -0,0 +1,88 @@
+package ubb.scs.map.laborator_6nou.service;
+
+import ubb.scs.map.laborator_6nou.domain.Friendship;
+import ubb.scs.map.laborator_6nou.domain.User;
+import ubb.scs.map.laborator_6nou.domain.event.ChangeEventType;
+import ubb.scs.map.laborator_6nou.domain.event.FriendshipEntityChange;
+import ubb.scs.map.laborator_6nou.repository.FriendshipDBRepository;
+import ubb.scs.map.laborator_6nou.repository.UserDBRepository;
+import ubb.scs.map.laborator_6nou.utils.Observable;
+import ubb.scs.map.laborator_6nou.utils.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendshipService implements Service<Friendship>, Observable<FriendshipEntityChange> {
+    FriendshipDBRepository repository;
+    UserDBRepository userRepository;
+    private List<Observer<FriendshipEntityChange>> observers=new ArrayList<>();
+
+    public FriendshipService(FriendshipDBRepository repository, UserDBRepository userRepository) {
+        this.repository = repository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Friendship delete(Long ID) {
+        Friendship a=repository.delete(ID).orElseThrow(() -> new IllegalArgumentException("Service says: invalid ID"));
+        FriendshipEntityChange change=new FriendshipEntityChange(ChangeEventType.DELETE,a);
+        notifyObservers(change);
+        return a;
+    }
+
+    public Friendship save(Long ID1, Long ID2) {
+        Friendship friendship = new Friendship(ID1, ID2);
+        Friendship a=repository.save(friendship).orElse(null);
+        FriendshipEntityChange event=new FriendshipEntityChange(ChangeEventType.ADD, friendship);
+        notifyObservers(event);
+        return a;
+    }
+
+    @Override
+    public Iterable<Friendship> findAll() {
+        return repository.findAll();
+    }
+    public ArrayList<User> getFriends(Long ID){
+        ArrayList<User> friends = new ArrayList<>();
+        Iterable<Friendship> friendships = repository.findAll();
+        friendships.forEach(friendship->{
+            if(friendship.getFirstFriend().equals(ID)) {
+                User friend=userRepository.findOne(friendship.getSecondFriend()).orElseThrow(() -> new IllegalArgumentException("User not found"));
+                friends.add(friend);
+            }
+            else if(friendship.getSecondFriend().equals(ID)) {
+                User friend=userRepository.findOne(friendship.getFirstFriend()).orElseThrow(() -> new IllegalArgumentException("User not found"));
+                friends.add(friend);
+            }
+        });
+        return friends;
+    }
+    public Friendship findOne(Long ID1,Long ID2){
+        Iterable<Friendship> friendships = repository.findAll();
+        for(Friendship friendship:friendships){
+            if(friendship.getFirstFriend().equals(ID1) && friendship.getSecondFriend().equals(ID2)) {
+                return friendship;
+            }
+            if(friendship.getSecondFriend().equals(ID1) && friendship.getFirstFriend().equals(ID2)) {
+                return friendship;
+            }
+        }
+        return null;
+    }
+    @Override
+    public void addObserver(Observer<FriendshipEntityChange> e) {
+        observers.add(e);
+
+    }
+
+    @Override
+    public void removeObserver(Observer<FriendshipEntityChange> e) {
+        //observers.remove(e);
+    }
+
+    @Override
+    public void notifyObservers(FriendshipEntityChange t) {
+
+        observers.stream().forEach(x->x.update(t));
+    }
+}
