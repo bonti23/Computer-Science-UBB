48c021bf603f343d1188dc742123d00305516db4
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-01-08 14:32:22 +0200

    Update 1a.pl

--- 3rd Semester/Logical and Functional Programming/labs/prolog/p1/1st exercise/1a.pl
+++ 3rd Semester/Logical and Functional Programming/labs/prolog/p1/1st exercise/1a.pl
@@ -1,9 +1,9 @@
-apartine_lista(_, []) :- false, !.
-apartine_lista(X, [X|_]) :- !.
-apartine_lista(X, [_|T]) :- apartine_lista(X, T).
+apartine_lista(_, []):-false,
+    !.
+apartine_lista(X, [X|_]):-!.
+apartine_lista(X, [_|T]):-apartine_lista(X, T).
 
 diferenta([], _, []):-!.
-diferenta([H|T], L2, R):- apartine_lista(H, L2), !,
-                           diferenta(T, L2, R).
-diferenta([H|T], L2, R):- diferenta(T, L2, R1),
-                          R=[H|R1].
+diferenta([H|T], L2, R):-apartine_lista(H, L2), !,
+                         diferenta(T, L2, R).
+diferenta([H|T], L2, [H|R]):-diferenta(T, L2, R).
