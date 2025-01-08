076accce366fd5918f58d1855daa5e5beb4b9127
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-01-08 17:04:51 +0200

    Update 1b.pl

--- 3rd Semester/Logical and Functional Programming/labs/prolog/p1/1st exercise/1b.pl
+++ 3rd Semester/Logical and Functional Programming/labs/prolog/p1/1st exercise/1b.pl
@@ -1,6 +1,9 @@
-adauga_par([],[]).
-adauga_par([H|T], [H,1|R]):-
-    H mod 2 =:= 0,
-    !,
-    adauga_par(T, R).
-adauga_par([H|T], [H|R]):-adauga_par(T, R).
+%adauga_par(l1...ln)={[], if l1...ln is vida
+%                    {l1 U 1 U adauga_par(l2...ln), if l1 e par
+%                    {l1 U adauga_par(l2...ln), altfel
+
+adauga_par([], []):-!.
+adauga_par([H|T], [H, 1|R]):-H mod 2 =:= 0,
+                            !,
+                            adauga_par(T, R).
+adauga_par([H|T], [H|R]):- adauga_par(T, R).
