0053cbd11fff6b3dfc9aaf32ca829cabb627620b
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-01-08 16:56:11 +0200

    Update 1a.pl

--- 3rd Semester/Logical and Functional Programming/labs/prolog/p1/1st exercise/1a.pl
+++ 3rd Semester/Logical and Functional Programming/labs/prolog/p1/1st exercise/1a.pl
@@ -1,7 +1,15 @@
+%apartine_lista(X, l1...ln)={false, if l1...ln e vida
+%                           {true, if l1=X
+%                           {apartine_lista(l2...ln), altfel
+
 apartine_lista(_, []):-false,
     !.
 apartine_lista(X, [X|_]):-!.
 apartine_lista(X, [_|T]):-apartine_lista(X, T).
+
+% diferenta(l1...ln, m1...mn)={diferenta(l2...ln, m1...mn), if
+% apartine_lista(l1, m1...mn)
+%                             {l1 U diferenta(l2...ln, m1...mn), altfel
 
 diferenta([], _, []):-!.
 diferenta([H|T], L2, R):-apartine_lista(H, L2), !,
