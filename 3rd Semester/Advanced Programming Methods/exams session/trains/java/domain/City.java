61f5234e4bce1e15674d6a98c369466ad45d6db1
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-02-23 11:24:24 +0200

    Create City.java

---
+++ 3rd Semester/Advanced Programming Methods/exams session/trains/java/domain/City.java
@@ -0,0 +1,32 @@
+package ubb.scs.map.examen.domain;
+
+import java.util.Objects;
+
+public class City extends Entity<String> {
+    private String name;
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
