af672607d02c69da151f89e490afd4e54a15724f
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-14 20:02:47 +0200

    Create ValidationException.java

---
+++ 3rd Semester/Advanced Programming Methods/exams session/holiday/java/domain/validation/ValidationException.java
@@ -0,0 +1,7 @@
+package ubb.scs.map.vacante.domain.validation;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
