e20af11b082d4a07bb3c6edc9718f60043cbc36e
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-14 19:16:29 +0200

    Create StackContainer.java

---
+++ 3rd Semester/Advanced Programming Methods/labs/2nd lab/model/StackContainer.java
@@ -0,0 +1,22 @@
+package sem1_2.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackContainer extends AbstractContainer {
+    List<Task> tasks = new ArrayList<>();
+
+    @Override
+    public Task remove() {
+        if (!tasks.isEmpty()) {
+            return tasks.remove(tasks.size() - 1);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void add(Task task) {
+        tasks.add(task);
+    }
+}
