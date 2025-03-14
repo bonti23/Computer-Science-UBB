f9dee3be0112cfe207b216a13598ec8857329124
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-14 19:58:16 +0200

    Create Entity.java

---
+++ 3rd Semester/Advanced Programming Methods/exams session/holiday/java/domain/Entity.java
@@ -0,0 +1,13 @@
+package ubb.scs.map.vacante.domain;
+
+public class Entity<ID> {
+    private ID identityKey;
+
+    public ID getID() {
+        return identityKey;
+    }
+
+    public void setID(ID identityKey) {
+        this.identityKey = identityKey;
+    }
+}
