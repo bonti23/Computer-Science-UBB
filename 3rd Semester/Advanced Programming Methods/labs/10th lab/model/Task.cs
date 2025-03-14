9a279249106f1f3420fca1658a512e7f3eac16e7
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-14 19:05:09 +0200

    Create Task.cs

---
+++ 3rd Semester/Advanced Programming Methods/labs/10th lab/model/Task.cs
@@ -0,0 +1,51 @@
+namespace Seminar_10;
+
+public abstract class Task
+{
+    private string taskID;
+    private string description;
+
+    protected Task(string taskID, string description)
+    {
+        this.taskID = taskID;
+        this.description = description;
+    }
+
+    public string Description
+    {
+        get {return description;}
+        set {description = value;}
+    }
+
+    public string TaskID
+    {
+        get {return taskID;}
+        set {taskID = value;}
+    }
+
+    protected bool Equals(Task other)
+    {
+        return taskID == other.taskID && description == other.description;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Task)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(taskID, description);
+    }
+
+    public override string ToString()
+    {
+        return $" + taskID {taskID}, Description: {description}";
+    }
+
+    public abstract void Execute();
+      
+}
