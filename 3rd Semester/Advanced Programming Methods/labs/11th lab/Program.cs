9dae3f63e68505235169f02dff6924792195bd16
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-15 00:14:45 +0200

    Create Program.cs

---
+++ 3rd Semester/Advanced Programming Methods/labs/11th lab/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NBA_League.ui;
+
+namespace NBA_League
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI console = new UI();
+            console.Strart();
+        }
+    }
+}
