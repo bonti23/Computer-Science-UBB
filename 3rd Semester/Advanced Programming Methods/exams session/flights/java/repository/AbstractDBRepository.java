f08bd8ca7a793a74f9a303a99865623595fb8937
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-14 19:44:45 +0200

    Create AbstractDBRepository.java

---
+++ 3rd Semester/Advanced Programming Methods/exams session/flights/java/repository/AbstractDBRepository.java
@@ -0,0 +1,39 @@
+package ubb.scs.map.zboruri.repository;
+
+import ubb.scs.map.zboruri.domain.Entity;
+import ubb.scs.map.zboruri.domain.validation.Validation;
+
+import java.util.Optional;
+
+public abstract class AbstractDBRepository <ID,E extends Entity<ID>> implements NewRepository<ID,E> {
+    private final String url;
+    private final String username;
+    private final String password;
+    private final Validation<E> validator;
+
+    public AbstractDBRepository(String url, String username, String password, Validation<E> validator) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.validator = validator;
+    }
+    public abstract Optional<E> findOne(ID id);
+
+    public abstract Iterable<E> findAll();
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Validation<E> getValidator() {
+        return validator;
+    }
+}
