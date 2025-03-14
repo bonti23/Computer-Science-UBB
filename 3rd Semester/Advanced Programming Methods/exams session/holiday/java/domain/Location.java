7fdf6fcdd5bb0c052203b5f2f6c55ca4eaa73476
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-14 19:59:39 +0200

    Create Location.java

---
+++ 3rd Semester/Advanced Programming Methods/exams session/holiday/java/domain/Location.java
@@ -0,0 +1,16 @@
+package ubb.scs.map.vacante.domain;
+
+public class Location extends Entity<Long>{
+    private String name;
+    public Location(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
