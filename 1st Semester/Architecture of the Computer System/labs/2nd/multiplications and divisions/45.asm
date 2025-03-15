068dd1ebaeb1c732e2d86b440e6fe3461474c84b
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-15 17:31:33 +0200

    Create 45.asm

---
+++ 1st Semester/Architecture of the Computer System/labs/2nd/multiplications and divisions/45.asm
@@ -0,0 +1,40 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+; f*(e-2)/[3*(d-5)]
+; a, b, c, d - byte, e, f, g, h - word
+segment data use32 class=data
+    a db 2
+    b db 3
+    c db 4
+    d db 5
+    e dw 6
+    f dw 7
+    g dw 8
+    h dw 9
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov al, [d]
+        sub al, 5
+        mov bl, 3
+        mul bl
+        mov bx, ax
+        mov ax, [e]
+        sub ax, 2
+        mul word[f]
+        mov dx, 0
+        div bx
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
