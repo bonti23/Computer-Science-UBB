4c2647b8db21372f631b0cc4ace533ca1c4f2fd2
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-15 20:52:25 +0200

    Create 22.asm

---
+++ 1st Semester/Architecture of the Computer System/labs/5th lab/22.asm
@@ -0,0 +1,48 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+; Two byte strings S1 and S2 of the same length are given. Obtain the string D where each element contains the minimum of the corresponding elements from S1 and S2.
+segment data use32 class=data
+    s1 db 1, 3, 6, 2, 3, 7
+    len1 equ $-s1
+    s2 db 6, 3, 8, 1, 2, 5
+    len2 equ $-s2
+    d resb len1
+    
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov esi, 0
+        mov edi, 0
+        mov ebp, 0
+        mov ecx, len1
+        repeat:
+              mov al, [s1+esi]
+              mov bl, [s2+ebp]
+              cmp al, bl
+              jbe addA
+              jmp addB
+              addA:
+                    mov [d+edi], al
+                    inc edi
+              jmp skip
+              addB:
+                    mov [d+edi], bl
+                    inc edi
+              skip:
+                    inc esi
+                    inc ebp
+        loop repeat
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
