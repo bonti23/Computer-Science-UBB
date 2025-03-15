7673a464e9bfeaaf2543d9ce51a372311235412b
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-15 19:50:09 +0200

    Create 4.asm

---
+++ 1st Semester/Architecture of the Computer System/labs/5th lab/4.asm
@@ -0,0 +1,46 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+; Two byte strings S1 and S2 are given, having the same length. Obtain the string D in the following way: each element found on the even positions of D is the sum of the corresponding elements from S1 and S2, and each element found on the odd positions of D is the difference of the corresponding elements from S1 and S2. 
+segment data use32 class=data
+    s1 db 1, 2, 3, 4
+    len1 equ $-s1
+    s2 db 5, 6, 7, 8
+    len2 equ $-s2
+    d resb (len1+len2)/2
+    
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov esi, 0
+        mov ebp, 0
+        mov edi, 0
+        mov ecx, len1
+        repeat:
+              mov al, [s1+esi]
+              mov bl, [s2+ebp]
+              test esi, 01h
+              jz even
+              sub al, bl
+              jmp skip
+              even:
+                    add al, bl
+              skip:
+                    mov [d+edi], al
+                    inc edi
+                    inc esi
+                    inc ebp
+        loop repeat
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
