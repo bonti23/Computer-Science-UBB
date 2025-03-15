1247f70108681362a354783668e4731ce2a997fb
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-15 20:17:30 +0200

    Create 12.asm

---
+++ 1st Semester/Architecture of the Computer System/labs/5th lab/12.asm
@@ -0,0 +1,52 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+; Two character strings S1 and S2 are given. Obtain the string D by concatenating the elements found on even positions in S2 and the elements found on odd positions in S1.
+segment data use32 class=data
+    s1 db "abcde"
+    len1 equ $-s1
+    s2 db 1, 2, 3, 4, 5
+    len2 equ $-s2
+    d resb len1+len2
+    
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov esi, 0
+        mov edi, 0
+        mov ebp, 0
+        mov ecx, len2
+        repeat:
+              mov al, [s+ebp]
+              test ebp, 01h
+              jz false
+              mov [d+edi], al
+              inc edi
+              false:
+                    inc ebp
+        loop repeta
+        mov ecx, len1
+        repeat2:
+              mov al, [s1+esi]
+              test esi, 01h
+              jz ok
+              jmp skip
+              ok:
+                    mov [d+edi], al
+                    inc edi
+              skip:
+                    inc esi
+        loop repeat2
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
