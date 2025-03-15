cd91cb927ca87dba475be4aad2a407e13a4c707a
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-03-15 22:10:06 +0200

    Delete 4th Semester/Artificial Intelligence/labs/1st lab/1st exercise directory

--- 4th Semester/Artificial Intelligence/labs/1st lab/1st exercise/1eu.py
+++
@@ -1,8 +0,0 @@
-def functie(sir):
-    cuvinte = [cuv for cuv in sir.split(" ")]
-    sortat = sorted(cuvinte)
-    return sortat[len(sortat) - 1]
-
-assert(functie("ana are mere rosii si galbene")=="si")
-sir = input("Introduceti sirul: ")
-print(functie(sir))
--- 4th Semester/Artificial Intelligence/labs/1st lab/1st exercise/2eu.py
+++
@@ -1,9 +0,0 @@
-import math
-def functie(a, b, c, d):
-    #sqrt((x2-x1)^2+(y2-y1)^2)
-    return math.sqrt(pow(c-a,2)+pow(d-b,2))
-
-assert(functie(1,5,4,1)==5.0)
-a, b = map(int, input("Introduceti prima pereche: ").split(','))
-c, d = map(int, input("Introduceti a doua pereche: ").split(','))
-print(functie(a, b, c, d))
