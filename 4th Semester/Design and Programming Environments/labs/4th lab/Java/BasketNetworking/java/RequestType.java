29f5cd990d6a5daf0b36b18e04955ea3e2bdf339
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-05-06 20:42:31 +0300

    Create RequestType.java

---
+++ 4th Semester/Design and Programming Environments/labs/4th lab/Java/BasketNetworking/java/RequestType.java
@@ -0,0 +1,12 @@
+package ro.mpp2024;
+
+public enum RequestType {
+    LOGIN,
+    LOGOUT,
+    SIGNUP,
+    FILTER_GAMES,
+    ADD_PURCHASE,
+    FIND_BY_CLIENT,
+    UPDATE_SEATS,
+    CHECK_USERNAME
+}
