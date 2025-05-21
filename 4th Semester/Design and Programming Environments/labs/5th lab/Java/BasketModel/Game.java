3791532fc19251dd79902f69e6c16a7b18873887
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-05-21 18:59:01 +0300

    Create Game.java

---
+++ 4th Semester/Design and Programming Environments/labs/5th lab/Java/BasketModel/Game.java
@@ -0,0 +1,89 @@
+package ro.mpp2024;
+
+import java.io.Serializable;
+
+public class Game extends Entity<Long> implements Serializable {
+    private String teamA;
+    private String teamB;
+    private String date;
+    private float price;
+    private Type type;
+    private int seats;
+
+    public Game(Long id, String teamA, String teamB, String date, float price, Type type, int seats) {
+        super.set_identitykey(id);
+        this.teamA = teamA;
+        this.teamB = teamB;
+        this.date = date;
+        this.price = price;
+        this.type = type;
+        this.seats = seats;
+    }
+
+
+    public Long get_identitykey(){
+        return super.get_identitykey();
+    }
+    public void set_identitykey(Long id){
+        super.set_identitykey(id);
+    }
+    public String getTeamA() {
+        return teamA;
+    }
+
+    public void setTeamA(String teamA) {
+        this.teamA = teamA;
+    }
+
+    public String getTeamB() {
+        return teamB;
+    }
+
+    public void setTeamB(String teamB) {
+        this.teamB = teamB;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "teamA='" + teamA + '\'' +
+                ", teamB='" + teamB + '\'' +
+                ", date='" + date + '\'' +
+                ", price=" + price +
+                ", type=" + type +
+                ", seats=" + seats +
+                '}';
+    }
+}
