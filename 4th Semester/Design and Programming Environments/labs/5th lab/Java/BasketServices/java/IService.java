10c6eab8acc529886318970297bb1f1369d8644e
Alexandra-Mihaela Bonțidean <155551124+bonti23@users.noreply.github.com>
2025-05-22 18:10:26 +0300

    Create IService.java

---
+++ 4th Semester/Design and Programming Environments/labs/5th lab/Java/BasketServices/java/IService.java
@@ -0,0 +1,22 @@
+package ro.mpp2024;
+
+import java.util.List;
+import java.util.Optional;
+import ro.mpp2024.IObserver;
+
+
+public interface IService {
+    User login(String username, String password, IObserver observer)throws Exception;
+    void logout(int id, IObserver client);
+    void signup(String name, String username, String password);
+    List<Purchase> find_by_client(String client);
+    List<Game> showcase_games_by_type(String type);
+    void add_purchase(Purchase purchase) throws Exception;
+    Game update_seats(Game game, int seats);
+    void validatePassword(String password);
+    List<Game> show_games();
+    boolean find_by_username(String username);
+    List<Game> all_games();
+    List<Purchase> findAllPurchases();
+    List<User> findAllUsers();
+}
